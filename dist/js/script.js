18f4613bef0048c15f4e1b90bd20f6e4cd85b33a
Tamara <tamara.herashchenko@gmail.com>
2021-11-29 13:18:02 +0200

    update dist

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-/* При клике на гамбургер открывается меню, при клике на крестик меню закрывается */
+/* hamburger */
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
@@ -103,35 +103,13 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
   menu.classList.remove('active');
 });
-/* const hamburger - создаём переменную
-document - обращаемся к index.html 
-querySelector - получаем элемент по селектору .hamburger
-menu = document.querySelector('.menu') - обращаемся к классу меню
-closeElem = document.querySelector('.menu__close'); - обращаемся к крестику close 
-
-hamburger.addEventListener('click', () => - отслеживаем все клики по гамбургеру
-menu.classList.add('active'); - после клика обращаемся к списку классов меню и выбираем класс активности
-
-menu.classList.remove('active'); - убираем класс активности */
-
-/* При изменении процентов, линия автоматически заполняется на это число
- */
+/* percents */
 
 const percents = document.querySelectorAll('.scale__item-percent');
 lines = document.querySelectorAll('.scale__item-progress span');
 percents.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
-/* const percents - создаём переменную
-document - обращаемся к index.html 
-querySelector - получаем элемент по селектору .scale__item-percent
-lines = document.querySelectorAll('.scale__item-progress span'); - создаем еще одну переменную
-querySelectorAll - получем все элементы по определенному селектору .scale__item-progress span
-
-percents.forEach( (item,i) => { - каждый элемент, который перебирается
-lines[i].style.width = item.innerHTML; - обращаемся к лайнс, 
-для выбора конкретного номера - i; 
-в этом конкретном элементе подставляем style.width, которое вытаскиваем с item.innerHTML (содержимое) */
 
 /***/ })
 
