479a9ef24b661c294d2c9942c4730ee12304a705
Tamara <tamara.herashchenko@gmail.com>
2021-12-06 13:23:08 +0200

    Add smoothScroll for script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -105,11 +105,40 @@ closeElem.addEventListener('click', () => {
 });
 /* percents */
 
-const percents = document.querySelectorAll('.scale__item-percent');
-lines = document.querySelectorAll('.scale__item-progress span');
+const percents = document.querySelectorAll('.scale__item-percent'),
+      lines = document.querySelectorAll('.scale__item-progress span');
 percents.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
 });
+/* scroll to a section */
+
+const promo = document.getElementById('promo'),
+      about = document.getElementById('about'),
+      benefit = document.getElementById('benefit'),
+      skills = document.getElementById('skills'),
+      portfolio = document.getElementById('portfolio'),
+      contact = document.getElementById('contact'),
+      menuLinks = document.querySelectorAll('.menu__link'),
+      promoLinks = document.querySelectorAll('.promo__link');
+
+function smoothScroll(wrapper, section, numberOfLink) {
+  wrapper[numberOfLink].addEventListener('click', e => {
+    e.preventDefault();
+    menu.classList.remove('active');
+    section.scrollIntoView({
+      block: 'start',
+      behavior: "smooth"
+    });
+  });
+}
+
+smoothScroll(menuLinks, about, 0);
+smoothScroll(menuLinks, benefit, 1);
+smoothScroll(menuLinks, skills, 2);
+smoothScroll(menuLinks, portfolio, 3);
+smoothScroll(menuLinks, contact, 4);
+smoothScroll(promoLinks, portfolio, 0);
+smoothScroll(promoLinks, about, 1);
 
 /***/ })
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 /* hamburger */
 
 const hamburger = document.querySelector('.hamburger'), 
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
     
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -13,11 +13,38 @@ closeElem.addEventListener('click', () => {
 });
 
 /* percents */
-const percents = document.querySelectorAll('.scale__item-percent');
-    lines = document.querySelectorAll('.scale__item-progress span');
+const percents = document.querySelectorAll('.scale__item-percent'),
+      lines = document.querySelectorAll('.scale__item-progress span');
 
 percents.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
-
+      lines[i].style.width = item.innerHTML;
 });
+
+/* scroll to a section */
+const promo = document.getElementById('promo'),
+      about = document.getElementById('about'),
+      benefit = document.getElementById('benefit'),
+      skills = document.getElementById('skills'),
+      portfolio = document.getElementById('portfolio'),
+      contact = document.getElementById('contact'),
+
+      menuLinks = document.querySelectorAll('.menu__link'),
+      promoLinks = document.querySelectorAll('.promo__link');
+
+function smoothScroll (wrapper, section, numberOfLink) {
+    wrapper[numberOfLink].addEventListener('click', (e) => {
+        e.preventDefault();
+        menu.classList.remove('active');
+        section.scrollIntoView({block:'start', behavior: "smooth"});
+    });
+}
+
+smoothScroll(menuLinks, about, 0);
+smoothScroll(menuLinks, benefit, 1);
+smoothScroll(menuLinks, skills, 2);
+smoothScroll(menuLinks, portfolio, 3);
+smoothScroll(menuLinks, contact, 4);
+smoothScroll(promoLinks, portfolio, 0);
+smoothScroll(promoLinks, about, 1);
+
 
